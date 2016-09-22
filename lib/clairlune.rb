8a80513be4f1b58dd079d3d51dfac6bd35d770d6
mozamimy (Moza USANE) <mozamimy@quellencode.org>
2016-09-22 22:50:59 +0900

    Improve build option names

--- lib/clairlune.rb
+++ lib/clairlune.rb
@@ -6,12 +6,12 @@ require 'clairlune/version'
 
 module Clairlune
   class Builder
-    def initialize(key:, bucket:, package_json_file_path:, build_function_name:, destination:, loglevel: 'info')
+    def initialize(key:, bucket:, package_json:, function_name:, dest:, loglevel: 'info')
       @key = key
       @bucket = bucket
-      @build_function_name = build_function_name
-      @destination = destination
-      @package_json = File.read(package_json_file_path)
+      @function_name = function_name
+      @dest = dest
+      @package_json = File.read(package_json)
       @logger = Logger.new(STDOUT)
       @logger.level = loglevel
     end
@@ -19,10 +19,10 @@ module Clairlune
     def performe(loglevel: 'info')
       lambda_client = Aws::Lambda::Client.new
 
-      @logger.info("Invoke build function: #{@build_function_name}")
+      @logger.info("Invoke build function: #{@function_name}")
 
       lambda_response = lambda_client.invoke(
-        function_name: @build_function_name,
+        function_name: @function_name,
         payload: @package_json,
         invocation_type: 'RequestResponse',
       )
@@ -49,7 +49,7 @@ module Clairlune
       @logger.info("Download zipped npm module from: s3://#{@bucket}/#{@key}")
 
       s3_client = Aws::S3::Client.new
-      File.open(@destination, 'w') do |file|
+      File.open(@dest, 'w') do |file|
         s3_client.get_object(bucket: @bucket, key: @key) do |chunk|
           file.write(chunk)
         end
@@ -57,7 +57,7 @@ module Clairlune
 
       {
         success: true,
-        file_path: @destination,
+        file_path: @dest,
         description: 'success',
       }
     end
